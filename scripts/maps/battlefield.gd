a0f0d88e1bb724e0d74caaf45ad02abdde5e5337
Sylvain Berthaud <sylvain.berthaud@gmail.com>
2025-10-12 22:20:37 +0200

    Signed-off-by: Sylvain Berthaud <sylvain.berthaud@gmail.com>

--- scripts/maps/battlefield.gd
+++ scripts/maps/battlefield.gd
@@ -3,9 +3,16 @@ extends Node
 @onready var ui = $UI
 @onready var player_profile = $PlayerProfileUI as PlayerProfileUI
 var xp_gain = 50
-const JunkRaft = preload("res://scripts/maps/junk_raft.gd")
 
+# Références aux paths et islands
+@onready var player_path = $player_path
+@onready var enemy_path = $ennemi_path
+@onready var player_path_follow = $player_path/PathFollow2D
+@onready var enemy_path_follow = $ennemi_path/PathFollow2D
 @onready var player_island = $player_path/PathFollow2D/Player_island
+@onready var enemy_island = $ennemi_path/PathFollow2D/ennemi_island
+
+# Système de swipe
 var player_raft: JunkRaft
 
 func _ready():
@@ -18,36 +25,47 @@ func _ready():
 	
 	# S'assurer que l'UI de fin est cachée au départ
 	ui.visible = false
+	
+	# Configurer le système de swipe
 	setup_swipe_system()
 
 func setup_swipe_system():
+	"""Configure le système de swipe pour l'île du joueur"""
 	var raft = JunkRaft.new()
 	raft.name = "SwipeController"
 	player_island.add_child(raft)
 	player_raft = raft
 	player_raft.start_combat()
+	print("✅ Système de swipe activé")
 
-func _process(_delta):
-	# Vérifier la condition de victoire/défaite
+func _process(delta):
+	# Déplacer l'île du joueur selon la vitesse du swipe
+	if player_raft:
+		player_path_follow.progress += player_raft.current_speed * delta
 	
+	# L'ennemi garde sa vitesse fixe
+	enemy_path_follow.progress += 75 * delta
+	
+	# Vérifier les conditions de victoire/défaite
+	check_victory_conditions()
+
+func check_victory_conditions():
+	"""Vérifie si la bataille est terminée"""
+	if ui.visible:
+		return  # Déjà terminé
 	
 	var player_tiles = get_tree().get_nodes_in_group("player")
 	var enemy_tiles = get_tree().get_nodes_in_group("enemy")
-
-	if player_raft:
-		player_path_follow.progress += player_raft.current_speed * _delta
 	
-	# L'ennemi garde sa vitesse fixe
-	enemy_path_follow.progress += 75 * _delta
-
-	if player_tiles.size() == 0 and not ui.visible:
+	if player_tiles.size() == 0:
 		end_game("enemy")
-	elif enemy_tiles.size() == 0 and not ui.visible:
+	elif enemy_tiles.size() == 0:
 		end_game("player")
 
 func end_game(winner: String):
+	"""Termine la partie"""
 	get_tree().paused = true
-
+	
 	var message = ""
 	if winner == "player":
 		# Ajouter l'XP au profil du joueur
